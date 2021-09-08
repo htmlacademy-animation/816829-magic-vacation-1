fb5a6eaa843348a991b30266d01353cba9fe607b
Andrey <ampu@yandex.ru>
2021-09-08 09:15:56 +0300

    fixup! Выполняет задание 5 - Добавляем анимацию блоков

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,7 @@ gulp.task("images", function() {
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
-
     .pipe(gulp.dest("source/img"));
-
 });
 
 gulp.task("webp", function () {
